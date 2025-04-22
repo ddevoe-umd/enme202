f69bb170bbd0a7e1c5663d5dee6c745dd43f5084
ddevoe-umd <84353435+ddevoe-umd@users.noreply.github.com>
2025-04-21 21:53:08 -0400

    Add files via upload

--- python/13_list_comprehension.py
+++ python/13_list_comprehension.py
@@ -26,7 +26,6 @@ print('---------------------------------------')
 # is how we might do this using a standard for loop:
 
 vals = [1, 2, 5, 7]
-print(vals)
 
 sq_vals = []
 for v in vals:
@@ -40,12 +39,13 @@ print(sq_vals)
 
 # List comprehension can be applied to any iterable object:
 
-print([v/2 for v in range(10)])  # range object as iterable
+new_list_1 = [v/2 for v in range(10)]  # range object as iterable
 
-print([tup for tup in enumerate(range(5,10))])          # enumerate object as iterable 
+new_list_2 = [tup for tup in enumerate(range(5,10))]    # enumerate object as iterable 
                                                         # and tuples as values
-print([idx%3 + x for idx, x in enumerate(range(15))])   # enumerate object as iterable, 
-                                                        # using separate idx,x values
+new_list_3 = [idx%3 + x for idx, x in enumerate(range(15))]  # enumerate object as iterable
+                                                             # (with unpacked tuple)
+new_list_4 = [k.upper() for k in {'a':1, 'b':2}]        # dictionary as iterable
 
 print()
 print('List Comprehension with Multiple Iterables:')
@@ -77,6 +77,13 @@ flat_array = [val for row in array for val in row]
 print(flat_array)
 
 
+# Another way to work with a multiple lists is using zip() which
+# "packages" paired values from each list as tuples:
+
+added_lists = [x+y for x,y in zip(v1, v2)]
+print(added_lists)
+
+
 print()
 print('List Comprehension with Conditionals:')
 print('---------------------------------------')
@@ -84,17 +91,44 @@ print('---------------------------------------')
 # Conditional statements can be included, allowing entry into the new list based
 # on an evaluation of the iterable value.
 
-# Example: create a list of numbers between [1,100) that are evenly dividible by 3: 
+# Case 1: use an _if_ condition to filter elements from iterable. In this case the
+# _if_ statement is placed *after* the iterable.
+#
+# Example: create a list of numbers in [1,99] that are evenly divisible by 3: 
+
 div_by_three = [x for x in range(1,100) if x%3 == 0]
 print(div_by_three)
 
+# Case 2: Don't filter the iterable, but use _if/else_ condition to decide
+# what to do with each value. In this case the _if/else_ statements are
+# placed *before* the iterable.
 
-print()
-print('PRACTICE PROBLEMS')
-print('---------------------------------------')
+div_by_three = [x if x%3 == 0 else '.' for x in range(1,100)]
+print(div_by_three)
 
-practice = """
-Easy
+# Do you see the difference? In the first case, the iterable list was filtered
+# to ignore values that did not meet the _if_ condition. In the second case,
+# all values from the iterable were included, but the _if/else_  condition
+# allowed us to decide what to do with each value.
+
+# Conditionals can also be nested in list comprehension. Here is an example that
+# tests each list element for divisibility by different values, with different 
+# entries in the final list depending on the result of each conditional:
+
+values = list(range(1,50))  
+result = [int(n/2) if n % 2 == 0 else int(n/3) if n % 3 == 0 else 'non-integer' for n in values]  
+print(result)  
+
+# Finally, conditionals can be used to both filter the iterable and decide how to process
+# individual values in the same list comprehension statement:
+
+div_by_three = [x if x%3 == 0 else '.' for x in range(1,100) if math.sin(x)>0.5]
+print(div_by_three)
+
+
+"""
+PRACTICE PROBLEMS
+
 1. Basic List Comprehension: Write a list comprehension to create a list of 
    squares of numbers from 1 to 10.
 2. Filter Even Numbers: Use a list comprehension to create a list of even 
@@ -103,11 +137,11 @@ Easy
    use a list comprehension to create a list of their lengths.
 4. Uppercase Conversion: Use a list comprehension to convert the list 
    ["hello", "world"] to ["HELLO", "WORLD"].
-
-Medium
-5. Conditional Filtering: Use a list comprehension to create a list of numbers 
-   from 1 to 50 that are divisible by 5.
-6. Extract Initials: Given a list of names use a list comprehension to extract 
+5. Names to list: Given a list of names, use list comprehension to create a new
+   list with each element being another list with 2 elements given by the first and
+   last names: [["James","Joyce", ["Gene","Wolfe"], ["George,Orwell"]] given the list 
+   ["James Joyce", "Gene Wolfe", "George Orwell"]
+6. Extract Initials: Given a list of names, use list comprehension to extract 
    the first letter of each name: ['JJ', 'GW', 'GO'] given the list 
    ["James Joyce", "Gene Wolfe", "George Orwell"]
 7. Flatten a Nested List: Given a nested list [[1, 2], [3, 4], [5, 6]], 
@@ -116,8 +150,6 @@ Medium
    (x, y) where x is from [1, 2, 3] and y is from [4, 5].
 9. Replace Negative Numbers: Given a list numbers = [-5, 3, -1, 2], use a list 
    comprehension to create a new list where all negative numbers are replaced by 0.
-   
-Hard
 10. Filter Words by Length: Given a list of words ["cat", "dog", "elephant", "fox"],
     use a list comprehension to create a new list containing only the words with 
     more than three letters.
@@ -134,10 +166,7 @@ Hard
 15. Prime Numbers: Use a list comprehension to generate a list of all prime 
     numbers between 2 and 50. (Hint: Use nested comprehensions or a helper 
     function for the primality check.)
-
 """
-print(practice)
-
 
 
 
